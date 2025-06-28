a06a611ff6595cb704883cbf58d94331d0827924
Vitor Andrade <vitor2.a.schweitzer@example.com>
2025-06-28 16:27:28 -0300

    preparativos pra deploy 15

--- backend/Web/settings.py
+++ backend/Web/settings.py
@@ -170,8 +170,8 @@ SIMPLE_JWT = {
     "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
 }
 
-MEDIA_URL = '/media/' 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_URL = '/media/' 
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
