115681b1597a0122c691aae4434708c8571567a4
gabpessi <155784317+gabpessi@users.noreply.github.com>
2025-06-29 19:52:53 -0300

    Delete backend directory

--- backend/Revela/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Post, UserProfile, Conversation, Message
-
-
-# Register your models here.
-admin.site.register(Post)
-admin.site.register(UserProfile)
-admin.site.register(Conversation)
-admin.site.register(Message)
